2dcfc4abb8db8ae3d3b4959ba03a0f8ba3d35614
Vinnicius Cedraz Ribeiro <vcedraz-@c1r6p3.42sp.org.br>
2022-10-13 14:27:35 -0300

    scripts updated

--- trip.sh
+++ trip.sh
@@ -4,5 +4,5 @@ echo "please type the lean name of the function, for example, \"calloc\"":
 
 read -r function
 
-# make "$function" -C libftTester
-make docker"$function" -C ~/libftTester/
+make "$function" -C ~/libftTester
+# make docker"$function" -C ~/libftTester/
