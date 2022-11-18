c27af665c17b4649d62a0eb9555964fdc43aaa47
Vinni-Cedraz <planetexpress0101@gmail.com>
2022-11-18 10:18:03 -0300

    strrev improved

--- ft_strrev.c
+++ ft_strrev.c
@@ -6,25 +6,24 @@
 /*   By: vcedraz- <vcedraz-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/01 18:28:57 by vcedraz-          #+#    #+#             */
-/*   Updated: 2022/11/16 18:01:15 by vcedraz-         ###   ########.fr       */
+/*   Updated: 2022/11/18 09:48:08 by vcedraz-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strrev(char *str)
+char	*ft_strrev(char *str, size_t strlen)
 {
 	t_ools	tool;
 
-	tool.i = -1;
-	tool.len = ft_strlen(str);
-	tool.half = tool.len / 2;
+	tool.frmbgn = -1;
+	tool.frmend = strlen;
 	tool.tmp = malloc(sizeof(char));
-	while (++tool.i < tool.half)
+	while (++tool.frmbgn < strlen/2)
 	{
-		*tool.tmp = *(str + tool.i);
-		*(str + tool.i) = *(--tool.len + str);
-		*(str + tool.len) = *tool.tmp;
+		*tool.tmp = *(str + tool.frmbgn);
+		*(str + tool.frmbgn) = *(--tool.frmend + str);
+		*(str + tool.frmend) = *tool.tmp;
 	}
 	return (free(tool.tmp), str);
 }
