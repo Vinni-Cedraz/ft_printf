3efb8c9d1a0fbebb460ae40c62e59669e1f71814
Vinni-Cedraz <planetexpress0101@gmail.com>
2022-11-18 10:18:41 -0300

    itoa changed to use new strrev and and to reduce the amount of numlens

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,7 +6,7 @@
 /*   By: vcedraz- <vcedraz-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/22 14:40:59 by vcedraz-          #+#    #+#             */
-/*   Updated: 2022/11/16 16:55:00 by vcedraz-         ###   ########.fr       */
+/*   Updated: 2022/11/18 10:16:21 by vcedraz-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,14 +22,15 @@ char	*ft_itoa(int n)
 		return (ft_strdup("0"));
 	if (n == INT_MIN)
 		return (ft_strdup("-2147483648"));
-	nb.to_ascii = ft_calloc(ft_numlen(n) + 1, sizeof(char));
+	nb.len = ft_numlen(n);
+	nb.to_ascii = ft_calloc(nb.len + 1, sizeof(char));
 	while (nb.abs != 0)
 	{
 		nb.to_ascii[nb.i++] = nb.abs % 10 + '0';
 		nb.abs /= 10;
 	}
 	if (n > 0)
-		return (ft_strrev(nb.to_ascii));
-	ft_strlcat(nb.to_ascii, "-", ft_numlen(n) + 2);
-	return (ft_strrev(nb.to_ascii));
+		return (ft_strrev(nb.to_ascii, nb.len));
+	ft_strlcat(nb.to_ascii, "-", nb.len + 1);
+	return (ft_strrev(nb.to_ascii, nb.len));
 }
