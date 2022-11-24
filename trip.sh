6546a6a893b4faba3b39a43089e0bf30a4fb2b1e
Vinni-Cedraz <planetexpress0101@gmail.com>
2022-11-24 17:32:36 -0300

    it' exactly as I planned, and no relinks, finally, the dot a files have to be separate tho

--- trip.sh
+++ trip.sh
@@ -2,5 +2,5 @@ echo "please type the lean name of the function, for example, \"calloc\"":
 
 read -r function
 
-make "$function" -C ~/home/libftTester
-# sudo make docker"$function" -C /home/vcedraz-/libftTester/
+# make "$function" -C ~/home/libftTester
+sudo make docker"$function" -C /home/vcedraz-/libftTester/
