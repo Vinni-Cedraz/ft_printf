0d9d1e154f6dc1298a91b47670cc890a4b13819c
Vinni-Cedraz <vcedraz-@student.42sp.org.br>
2023-03-17 14:30:20 -0300

    non-external-functions atoi

--- libft/ft_atoi.c
+++ libft/ft_atoi.c
@@ -5,73 +5,107 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: vcedraz- <vcedraz-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/09/20 13:49:17 by vcedraz-          #+#    #+#             */
-/*   Updated: 2023/02/02 12:48:09 by vcedraz-         ###   ########.fr       */
+/*   Created: 2023/03/17 14:27:22 by vcedraz-          #+#    #+#             */
+/*   Updated: 2023/03/17 14:29:01 by vcedraz-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static int			is_whitespace(char c);
-static int			isdigit(int c);
-static int			issign(int c);
+static inline int	how_mny_zeros(int diff);
+static inline int	is_whitespace(char c);
+static inline int	handle_negative(const char **str);
+static inline int	my_strlen(const char *str);
 
-int	ft_atoi(const char *asci)
+int	ft_atoi(const char *str)
 {
-	short	sign;
-	int		i;
-	int		to_int;
+	int	i;
+	int	res;
+	int	len;
+	int	sign;
 
-	i = 0;
+	i = -1;
+	res = 0;
 	sign = 1;
-	to_int = 0;
-	if (!asci)
+	while (is_whitespace(*str))
+		str++;
+	sign = handle_negative(&str);
+	len = my_strlen(str);
+	if (!len)
 		return (0);
-	while (is_whitespace(asci[i]))
-		i++;
-	if (issign(asci[i]))
+	while (++i < len)
+		res += ((str[i] - '0') * how_mny_zeros(len - i - 1));
+	return (res * sign);
+}
+
+static inline int	my_strlen(const char *str)
+{
+	const char	*end;
+
+	if (!str)
+		return (0);
+	end = str;
+	while (*end && !is_whitespace(*end))
 	{
-		sign = -1;
-		i++;
+		if ((*end < '0' || *end > '9') && *end != '-' && *end != '+')
+			return (0);
+		end++;
 	}
-	while (isdigit(asci[i]))
-	{
-		to_int = (to_int * 10) + (asci[i] - '0');
-		i++;
-	}
-	to_int *= sign;
-	return (to_int);
+	return (end - str);
 }
 
 static inline int	is_whitespace(char c)
 {
-	int					i;
-	static const char	whitespaces[]
-		= {SPACE,
-		CARRIAGE_RETURN,
-		NEWLINE,
-		TAB,
-		VERTICAL_TAB,
-		FORMFEED};
-
-	i = -1;
-	while (++i < 6)
-		if (c == whitespaces[i])
-			return (1);
-	return (0);
+	return (c == ' ' || ('\t' <= c && c <= '\r'));
 }
 
-static inline int	isdigit(int c)
+static inline int	how_mny_zeros(int diff)
 {
-	return ('0' <= c && c <= '9');
+	int	res;
+
+	res = 1;
+	while (diff)
+	{
+		res *= 10;
+		diff--;
+	}
+	return (res);
 }
 
-static inline int	issign(int c)
+static inline int	handle_negative(const char **str)
 {
-	if (c == '-')
-		return (-1);
-	else if (c == '+')
-		return (1);
-	else
-		return (0);
+	int	sign;
+
+	sign = 1;
+	if (**str == '-' || **str == '+')
+	{
+		if (**str == '-')
+			sign = -1;
+		(*str)++;
+	}
+	return (sign);
 }
+// #include <assert.h>
+// #include <stdio.h>
+//
+// int	main(void)
+// {
+// 	assert(ft_atoi("4") == atoi("4"));
+// 	assert(ft_atoi("  4") == atoi("  4"));
+// 	assert(ft_atoi("  4  ") == atoi("  4  "));
+// 	assert(ft_atoi("  4  5") == atoi("  4  5"));
+// 	assert(ft_atoi("1235") == atoi("1235"));
+// 	assert(ft_atoi("  1235") == atoi("  1235"));
+// 	assert(ft_atoi("0") == atoi("0"));
+// 	assert(ft_atoi("1") == atoi("1"));
+// 	assert(ft_atoi("-1") == atoi("-1"));
+// 	assert(ft_atoi("-101") == atoi("-101"));
+// 	assert(ft_atoi("-101") == atoi("-101"));
+// 	assert(ft_atoi("-2147483648") == atoi("-2147483648"));
+// 	assert(ft_atoi("2147483647") == atoi("2147483647"));
+// 	assert(ft_atoi("+123") == atoi("123"));
+// 	assert(ft_atoi("  +123") == atoi("123"));
+// 	assert(ft_atoi("abc1") == atoi("abc1"));
+// 	assert(ft_atoi("") == atoi(""));
+// 	ft_putstr_fd("All tests passed.", 1);
+// }
